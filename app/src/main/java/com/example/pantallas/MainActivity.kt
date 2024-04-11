90ddea7a0d80b8038d7169dd6b376fb6ebca6ca7
arturovicky <arturo_gm1337@hotmail.com>
2024-04-11 00:56:07 -0700

    funciones para abrir pantallas

--- app/src/main/java/com/example/pantallas/MainActivity.kt
+++ app/src/main/java/com/example/pantallas/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.pantallas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable.ClassLoaderCreator
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
+private lateinit var pantalla1 : Button
+private lateinit var pantalla2 : Button
+private lateinit var nombre : TextInputEditText
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pantalla1 = findViewById(R.id.pantalla1)
+        pantalla2 = findViewById(R.id.pantalla2)
+        nombre = findViewById(R.id.nombre)
+
+        pantalla1.setOnClickListener { llamarPantalla(Intent(this,MainActivity2::class.java)) }
+        pantalla2.setOnClickListener { llamarPantalla(Intent(this,MainActivity3::class.java)) }
     }
+
+    private fun llamarPantalla(intent: Intent) {
+        startActivity(intent)
+    }
+
+
 }

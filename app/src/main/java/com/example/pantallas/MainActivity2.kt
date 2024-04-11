0d3f1179be9faefa5dd199fd25d34e7b9d8c3351
arturovicky <arturo_gm1337@hotmail.com>
2024-04-11 09:48:43 -0700

    funciones para enviar mensajes entre pantallas

--- app/src/main/java/com/example/pantallas/MainActivity2.kt
+++ app/src/main/java/com/example/pantallas/MainActivity2.kt
@@ -2,10 +2,15 @@ package com.example.pantallas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
+private lateinit var salir : Button
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        salir = findViewById(R.id.salir)
+        salir.setOnClickListener { finish() }
     }
 }
--- app/src/main/java/com/example/pantallas/MainActivity3.kt
+++ app/src/main/java/com/example/pantallas/MainActivity3.kt
@@ -2,10 +2,24 @@ package com.example.pantallas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
+private lateinit var salir3 : Button
+private lateinit var texto : TextView
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        salir3= findViewById(R.id.salir3)
+        texto = findViewById(R.id.textView)
+        salir3.setOnClickListener { finish() }
+
+        val intent = intent
+
+        texto.text = intent.getStringExtra("nombre")
+
+
     }
 }
